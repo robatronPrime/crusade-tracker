8e21cb78afc4152b283f34534340eb7c7a694225
robertstow <roberts@hyve.com>
2025-04-08 17:01:12 +0100

    temp hide forces

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <>
       <h2>Web app built to create and track 40k crusade forces</h2>
-      <Forces />
+      {/* <Forces /> */}
     </>
   );
 }
