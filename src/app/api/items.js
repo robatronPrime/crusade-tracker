8dfb0af3a7d8c777356165aab80b7b607ab4af8a
robertstow <roberts@hyve.com>
2025-04-08 11:00:36 +0100

    items methods, id methods

--- src/app/api/items.js
+++ src/app/api/items.js
@@ -2,40 +2,31 @@ import dbConnect from "../../../lib/mongodb";
 import Item from "../../../models/Item";
 
 export default async function handler(req, res) {
-  const {
-    query: { id },
-    method,
-  } = req;
-
   await dbConnect();
 
+  const { method } = req;
+
   switch (method) {
-    case "PUT":
+    case "GET":
       try {
-        const item = await Item.findByIdAndUpdate(id, req.body, {
-          new: true,
-          runValidators: true,
-        });
-
-        if (!item) {
-          return res.status(400).json({ success: false });
-        }
-
-        res.status(200).json({ success: true, data: item });
+        const items = await Item.find({});
+        res.status(200).json({ success: true, data: items });
       } catch (error) {
         res.status(400).json({ success: false });
       }
       break;
 
-    case "DELETE":
+    case "POST":
       try {
-        const deleteItem = Item.deleteOne({ _id: id });
+        const item = Item.create(req.body);
+        res.status(201).json({ success: true, data: item });
       } catch (error) {
         res.status(400).json({ success: false });
       }
       break;
 
     default:
+      res.status(400).json({ success: false });
       break;
   }
 }
