bf5d1b7209e32cc7f7dcebc711fab17565436e9a
roberts <roberts@hyve.com>
2026-09-08 13:17:57 +0100

    Refactor ForceDetails component to improve import order and maintain consistency.

--- src/components/ForceDetails.tsx
+++ src/components/ForceDetails.tsx
@@ -1,7 +1,9 @@
+"use client";
+
+import Link from "next/link";
+import StatBlock from "./StatBlock";
 import { JSX, useState } from "react";
 import UnitQuickActions from "./UnitQuickActions";
-import StatBlock from "./StatBlock";
-import Link from "next/link";
 import ForceDeleteButton from "./ForceDeleteButton";
 
 interface ForceProps {
